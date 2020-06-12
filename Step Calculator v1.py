6d9a7e58d9f6f9a5eb7efedced659418629318aa
TejasG5 <64637256+TejasG5@users.noreply.github.com>
2020-06-12 13:33:50 +0530

    Add files via upload

---
+++ Step Calculator v1.py
@@ -0,0 +1,15 @@
+print("STEP CALCULATOR")
+
+
+distance = float(input("Distance to cover(in cm):"))
+diameter = float(input("Enter the diameter of the wheels(in cm):"))
+step_angle = float(input("Enter the step angle(in degree): "))
+
+
+cir = diameter*3.14
+steps_in_one_rev = 360/step_angle
+total_revolutions = distance/cir
+total_steps = steps_in_one_rev * total_revolutions
+print("The number of steps are " + str(total_steps))  
+print(str(total_revolutions))
+
